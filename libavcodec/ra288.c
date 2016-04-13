0cbf20a0374415fb06729c2fdfcc1df4b658d1bb
Alexandra Hájková <alexandra@khirnov.net>
2016-04-13 19:34:49 +0200

    ra288: convert to the new bitstream reader

--- libavcodec/ra288.c
+++ libavcodec/ra288.c
@@ -25,7 +25,7 @@
 #include "avcodec.h"
 #include "internal.h"
 #define BITSTREAM_READER_LE
-#include "get_bits.h"
+#include "bitstream.h"
 #include "ra288.h"
 #include "lpc.h"
 #include "celp_filters.h"
@@ -180,7 +180,7 @@ static int ra288_decode_frame(AVCodecContext * avctx, void *data,
     float *out;
     int i, ret;
     RA288Context *ractx = avctx->priv_data;
-    GetBitContext gb;
+    BitstreamContext bc;
 
     if (buf_size < avctx->block_align) {
         av_log(avctx, AV_LOG_ERROR,
@@ -197,11 +197,11 @@ static int ra288_decode_frame(AVCodecContext * avctx, void *data,
     }
     out = (float *)frame->data[0];
 
-    init_get_bits(&gb, buf, avctx->block_align * 8);
+    bitstream_init(&bc, buf, avctx->block_align * 8);
 
     for (i=0; i < RA288_BLOCKS_PER_FRAME; i++) {
-        float gain = amptable[get_bits(&gb, 3)];
-        int cb_coef = get_bits(&gb, 6 + (i&1));
+        float gain = amptable[bitstream_read(&bc, 3)];
+        int cb_coef = bitstream_read(&bc, 6 + (i & 1));
 
         decode(ractx, gain, cb_coef);
 
