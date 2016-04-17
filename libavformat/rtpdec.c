1434683241f423378f5485aab929e94e400e05b4
Alexandra Hájková <alexandra@khirnov.net>
2016-04-17 16:59:24 +0200

    rtp: convert to the new bitstream reader

--- libavformat/rtpdec.c
+++ libavformat/rtpdec.c
@@ -22,7 +22,7 @@
 #include "libavutil/mathematics.h"
 #include "libavutil/avstring.h"
 #include "libavutil/time.h"
-#include "libavcodec/get_bits.h"
+#include "libavcodec/bitstream.h"
 #include "avformat.h"
 #include "network.h"
 #include "srtp.h"
