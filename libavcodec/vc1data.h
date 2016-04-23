a7a4f4ce35641267a7db5e681c78eb780dd21310
Alexandra Hájková <alexandra@khirnov.net>
2016-04-23 13:23:08 +0200

    vc1: Convert to the new bitstream reader

--- libavcodec/vc1data.h
+++ libavcodec/vc1data.h
@@ -30,7 +30,9 @@
 
 #include <stdint.h>
 #include "libavutil/rational.h"
-#include "get_bits.h"
+
+#include "bitstream.h"
+#include "vlc.h"
 
 /** Table for conversion between TTBLK and TTMB */
 extern const int ff_vc1_ttblk_to_tt[3][8];
